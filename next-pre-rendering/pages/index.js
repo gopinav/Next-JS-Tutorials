2129736278dc666d38f077a2ff5d7dc875ccb20a
Vishwas <vishwas.g5@gmail.com>
2021-04-25 17:46:21 +0530

    Add Link to users

--- next-pre-rendering/pages/index.js
+++ next-pre-rendering/pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 function Home() {
-  return <h1>Next JS pre-rendering</h1>
+  return (
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href='/users'>
+        <a>Users</a>
+      </Link>
+    </>
+  )
 }
 
 export default Home
