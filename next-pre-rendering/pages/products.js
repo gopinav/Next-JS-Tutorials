3185e1ca6f6beedfbcee404135236a275eae1f5c
Vishwas <vishwas.g5@gmail.com>
2021-05-03 18:58:04 +0530

    Mock data

--- next-pre-rendering/pages/products.js
+++ next-pre-rendering/pages/products.js
@@ -20,12 +20,12 @@ export default ProductList
 
 export async function getStaticProps() {
   console.log('Generating / Regenerating ProductList')
-  const response = await fetch('http://localhost:4000/products')
-  const data = await response.json()
+  // const response = await fetch('http://localhost:4000/products')
+  // const data = await response.json()
 
   return {
     props: {
-      products: data
+      products: []
     },
     revalidate: 10
   }
