836f9e9568b3a81c505d3de0a767dd7cd1669d37
Vishwas <vishwas.g5@gmail.com>
2021-05-04 15:38:06 +0530

    Fetching paths for getStaticPaths

--- next-pre-rendering/pages/posts/[postId].js
+++ next-pre-rendering/pages/posts/[postId].js
@@ -26,12 +26,21 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await response.json()
+  const paths = data.map(post => {
+    return {
+      params: { postId: `${post.id}` }
+    }
+  })
+
   return {
-    paths: [
-      { params: { postId: '1' } },
-      { params: { postId: '2' } },
-      { params: { postId: '3' } }
-    ],
+    // paths: [
+    //   { params: { postId: '1' } },
+    //   { params: { postId: '2' } },
+    //   { params: { postId: '3' } }
+    // ],
+    paths,
     fallback: false
   }
 }
