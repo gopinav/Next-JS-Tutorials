6e6c92439df0f63de80bdd6cb0d720b17da395e1
Vishwas <vishwas.g5@gmail.com>
2021-05-09 17:54:30 +0530

    Stale data problem

---
+++ next-pre-rendering/pages/products/[productId].js
@@ -0,0 +1,42 @@
+import { useRouter } from 'next/router'
+
+function Product({ product }) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+  return (
+    <div>
+      <h2>
+        {product.id} {product.title} {product.price}
+      </h2>
+      <p>{product.description}</p>
+      <hr />
+    </div>
+  )
+}
+
+export default Product
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const response = await fetch(
+    `http://localhost:4000/products/${params.productId}`
+  )
+  const data = await response.json()
+  console.log(`Generating page for /posts/${params.postId}`)
+
+  return {
+    props: {
+      product: data
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { productId: '1' } }],
+    fallback: true
+  }
+}
---
+++ next-pre-rendering/pages/products/index.js
@@ -0,0 +1,31 @@
+function ProductList({ products }) {
+  return (
+    <>
+      <h1>List of products</h1>
+      {products.map(product => {
+        return (
+          <div key={product.id}>
+            <h2>
+              {product.id} {product.title} {product.price}
+            </h2>
+            <hr />
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default ProductList
+
+export async function getStaticProps() {
+  //   console.log('Generating / Regenerating ProductList')
+  const response = await fetch('http://localhost:4000/products')
+  const data = await response.json()
+
+  return {
+    props: {
+      products: data
+    }
+  }
+}
