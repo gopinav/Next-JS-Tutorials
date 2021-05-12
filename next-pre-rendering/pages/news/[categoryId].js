01664e1e2df6485854a9e4c0c1818d0dc0e159b3
Vishwas <vishwas.g5@gmail.com>
2021-05-12 15:53:58 +0530

    SSR with dynamic parameters

---
+++ next-pre-rendering/pages/news/[categoryId].js
@@ -0,0 +1,38 @@
+function ArticleListByCategory({ articles, category }) {
+  return (
+    <>
+      <h1>Showing news for category "{category}"</h1>
+      {articles.map(article => {
+        return (
+          <div key={article.id}>
+            <h2>
+              {article.id} {article.title}
+            </h2>
+            <p>{article.description}</p>
+            <hr />
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default ArticleListByCategory
+
+export async function getServerSideProps(context) {
+  const { params } = context
+  const { categoryId } = params
+  const response = await fetch(
+    `http://localhost:4000/news?category=${categoryId}`
+  )
+  const data = await response.json()
+
+  console.log(`Pre-rendering News Articles for category ${categoryId}`)
+
+  return {
+    props: {
+      articles: data,
+      category: categoryId
+    }
+  }
+}
