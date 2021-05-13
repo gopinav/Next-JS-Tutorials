cfd991e47d17e96151f647b05378926ff0c4f758
Vishwas <vishwas.g5@gmail.com>
2021-05-13 20:35:09 +0530

    Fix errors

--- next-pre-rendering/pages/dashboard.js
+++ next-pre-rendering/pages/dashboard.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 
-function Dashboard({ dashboardData }) {
+function Dashboard() {
   const [isLoading, setIsLoading] = useState(true)
-  const [dashboardData, setDashboardData] = useState(dashboardData)
+  const [dashboardData, setDashboardData] = useState(null)
   useEffect(() => {
     async function fetchDashboardData() {
       const response = await fetch('http://localhost:4000/dashboard')
@@ -19,6 +19,7 @@ function Dashboard({ dashboardData }) {
 
   return (
     <div>
+      <h2>Dashboard</h2>
       <h2>Posts - {dashboardData.posts}</h2>
       <h2>Likes - {dashboardData.likes}</h2>
       <h2>Followers - {dashboardData.followers}</h2>
