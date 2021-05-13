c2b276e473fffed2afa27f3e90cbc455c7cbcc5b
Vishwas <vishwas.g5@gmail.com>
2021-05-13 11:23:28 +0530

    Client side data fetching

---
+++ next-pre-rendering/pages/dashboard.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+
+function Dashboard({ dashboardData }) {
+  const [isLoading, setIsLoading] = useState(true)
+  const [dashboardData, setDashboardData] = useState(dashboardData)
+  useEffect(() => {
+    async function fetchDashboardData() {
+      const response = await fetch('http://localhost:4000/dashboard')
+      const data = await response.json()
+      setDashboardData(data)
+      setIsLoading(false)
+    }
+    fetchDashboardData()
+  }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  return (
+    <div>
+      <h2>Posts - {dashboardData.posts}</h2>
+      <h2>Likes - {dashboardData.likes}</h2>
+      <h2>Followers - {dashboardData.followers}</h2>
+      <h2>Following - {dashboardData.following}</h2>
+    </div>
+  )
+}
+
+export default Dashboard
