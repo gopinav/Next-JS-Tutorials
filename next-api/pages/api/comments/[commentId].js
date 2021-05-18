4637a7d2343cf1baa3749f630949fda7dea1478e
Vishwas <vishwas.g5@gmail.com>
2021-05-18 20:55:24 +0530

    Find instead of filter

--- next-api/pages/api/comments/[commentId].js
+++ next-api/pages/api/comments/[commentId].js
@@ -3,9 +3,7 @@ import { comments } from '../../../data/comments'
 export default function handler(req, res) {
   const { commentId } = req.query
   if (req.method === 'GET') {
-    const comment = comments.filter(
-      comment => comment.id === parseInt(commentId)
-    )
+    const comment = comments.find(comment => comment.id === parseInt(commentId))
     res.status(200).json(comment)
   } else if (req.method === 'DELETE') {
     const deletedComment = comments.find(
