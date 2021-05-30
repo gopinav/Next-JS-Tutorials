3a14b9a5a54b4aea3a94800a1f2d524ef4e87298
Vishwas <vishwas.g5@gmail.com>
2021-05-30 15:29:09 +0530

    Absolute imports and module paths

--- next-misc/pages/_app.js
+++ next-misc/pages/_app.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import '../styles/globals.css'
-import '../styles/layout.css'
+import Footer from '@/layout/Footer'
+import Header from '@/layout/Header'
+import 'styles/globals.css'
+import 'styles/layout.css'
 
 function MyApp({ Component, pageProps }) {
   return (
