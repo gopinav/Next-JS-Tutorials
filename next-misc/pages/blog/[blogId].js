c641b8bc1fc4dfae7f2334054a1572d92a1c1576
Vishwas <vishwas.g5@gmail.com>
2021-06-01 15:39:20 +0530

    Environment variables

--- next-misc/pages/blog/[blogId].js
+++ next-misc/pages/blog/[blogId].js
@@ -7,7 +7,10 @@ function Blog({ title, description }) {
         <title>{title}</title>
         <meta name='description' content={description} />
       </Head>
-      <h1 className='content'>Article</h1>
+      <h1 className='content'>
+        Env User {process.env.DB_USER} Password {process.env.DB_PASSWORD}{' '}
+        Analytics {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+      </h1>
     </>
   )
 }
@@ -15,6 +18,11 @@ function Blog({ title, description }) {
 export default Blog
 
 export async function getServerSideProps() {
+  const user = process.env.DB_USER
+  const password = process.env.DB_PASSWORD
+  console.log(
+    `Connecting to database with username ${user} and password ${password}`
+  )
   return {
     props: {
       title: 'Article Title',
