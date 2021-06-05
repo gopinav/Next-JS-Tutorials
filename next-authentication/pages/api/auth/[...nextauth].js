b34314cf747f870e478805ecf531a3ca0d11071c
Vishwas <vishwas.g5@gmail.com>
2021-06-05 16:47:27 +0530

    Configure nextauth

---
+++ next-authentication/pages/api/auth/[...nextauth].js
@@ -0,0 +1,11 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET
+    })
+  ]
+})
