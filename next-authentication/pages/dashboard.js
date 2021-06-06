5a8554ac5ee96aea96ab076248f8f7afde93c991
Vishwas <vishwas.g5@gmail.com>
2021-06-06 10:42:25 +0530

    Securing pages client-side

--- next-authentication/pages/dashboard.js
+++ next-authentication/pages/dashboard.js
@@ -1,4 +1,26 @@
+import { useState, useEffect } from 'react'
+import { getSession, signIn } from 'next-auth/client'
+
 function Dashboard() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession()
+      console.log({ session })
+      if (!session) {
+        signIn()
+      } else {
+        setLoading(false)
+      }
+    }
+
+    securePage()
+  }, [])
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
   return <h1>Dashboard page</h1>
 }
 
