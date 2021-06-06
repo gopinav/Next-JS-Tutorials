04d3fde585dfe5fba1680f61a17107704e3bd127
Vishwas <vishwas.g5@gmail.com>
2021-06-06 07:23:04 +0530

    useSession Hook

--- next-authentication/components/Navbar.js
+++ next-authentication/components/Navbar.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 function Navbar() {
+  const [session, loading] = useSession()
   return (
     <nav className='header'>
       <h1 className='logo'>
         <a href='#'>NextAuth</a>
       </h1>
-      <ul className='main-nav'>
+      <ul className={`main-nav ${!session && loading ? 'loading' : 'loaded'}`}>
         <li>
           <Link href='/'>
             <a>Home</a>
@@ -23,12 +25,32 @@ function Navbar() {
           </Link>
         </li>
 
-        <li>
-          <a href='#'>Sign In</a>
-        </li>
-        <li>
-          <a href='#'>Sign Out</a>
-        </li>
+        {!loading && !session && (
+          <li>
+            <Link href='/api/auth/signin'>
+              <a
+                onClick={e => {
+                  e.preventDefault()
+                  signIn('github')
+                }}>
+                Sign In
+              </a>
+            </Link>
+          </li>
+        )}
+        {session && (
+          <li>
+            <Link href='/api/auth/signout'>
+              <a
+                onClick={e => {
+                  e.preventDefault()
+                  signOut()
+                }}>
+                Sign Out
+              </a>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   )
