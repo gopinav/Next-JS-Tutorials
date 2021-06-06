4c837c5752999a0fe4b001e7c25e74c6a6d5caec
Vishwas <vishwas.g5@gmail.com>
2021-06-06 14:54:25 +0530

    NextAuth Provider

--- next-authentication/pages/_app.js
+++ next-authentication/pages/_app.js
@@ -1,13 +1,14 @@
+import { Provider } from 'next-auth/client'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 import '../components/Navbar.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </Provider>
   )
 }
 
