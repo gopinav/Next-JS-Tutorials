c705ae1b6ed8473af57aa91af6b230cff71ce731
Vishwas <vishwas.g5@gmail.com>
2021-06-06 16:24:54 +0530

    Server-side authentication

--- next-authentication/pages/_app.js
+++ next-authentication/pages/_app.js
@@ -5,7 +5,7 @@ import '../components/Navbar.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider>
+    <Provider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
     </Provider>
--- next-authentication/pages/blog.js
+++ next-authentication/pages/blog.js
@@ -1,5 +1,19 @@
-function Blog() {
-  return <h1>Blog page</h1>
+import { getSession, useSession } from 'next-auth/client'
+
+function Blog({ data }) {
+  const [session] = useSession()
+  console.log({ session })
+  return <h1>Blog page - {data}</h1>
 }
 
 export default Blog
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  return {
+    props: {
+      data: session ? 'List of 100 personalized blogs' : 'List of free blogs',
+      session
+    }
+  }
+}
