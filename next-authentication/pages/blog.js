f64f7b1e711c0cc85ac83fc6f5280c1f9e3eb516
Vishwas <vishwas.g5@gmail.com>
2021-06-07 08:22:41 +0530

    Securing Pages Server-side

--- next-authentication/pages/blog.js
+++ next-authentication/pages/blog.js
@@ -10,9 +10,19 @@ export default Blog
 
 export async function getServerSideProps(context) {
   const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=${encodeURIComponent(
+          'http:localhost:3000/blog'
+        )}`,
+        permanent: false
+      }
+    }
+  }
   return {
     props: {
-      data: session ? 'List of 100 personalized blogs' : 'List of free blogs',
+      data: 'List of 100 personalized blogs',
       session
     }
   }
